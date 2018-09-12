c1bfd7befd07ea61abf924653ce2813bf0f29b03
HybridRanger <hybridranger64@gmail.com>
2018-09-12 13:40:28 +0100

    added runtime debug buttons
    two buttons to test the folder searching during runtime
    - GetHeightmaps
    - GetOverlays

---
+++ Vuforia-AR-App/Assets/Scripts/GetFolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFolder : MonoBehaviour {
+
+    public void GetFolderContents(string folderName)
+    {
+        Debug.Log(folderName);
+    }
+
+}
