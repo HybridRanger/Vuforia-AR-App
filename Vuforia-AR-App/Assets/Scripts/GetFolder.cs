114e15c697cae92d3db56fa47c0db1bb3cc5531d
HybridRanger <hybridranger64@gmail.com>
2018-09-12 13:47:04 +0100

    list folder contents
    prints folder contents to console on button press.
    
    also prints the .meta files so will need to fix

--- Vuforia-AR-App/Assets/Scripts/GetFolder.cs
+++ Vuforia-AR-App/Assets/Scripts/GetFolder.cs
@@ -6,7 +6,12 @@ public class GetFolder : MonoBehaviour {
 
     public void GetFolderContents(string folderName)
     {
-        Debug.Log(folderName);
+        string path = "Assets/" + folderName;
+
+        foreach (string file in System.IO.Directory.GetFiles(path))
+        {
+            Debug.Log(file);
+        }
     }
 
 }
