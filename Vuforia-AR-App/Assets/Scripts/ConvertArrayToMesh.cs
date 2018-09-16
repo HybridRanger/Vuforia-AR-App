6cf2fb460bc0dff2b71e5e85aeeb6e088f8f04c2
HybridRanger <hybridranger64@gmail.com>
2018-09-16 20:27:37 +0100

    working on the terrain generation
    added convert image to array, convert array to image

---
+++ Vuforia-AR-App/Assets/Scripts/ConvertArrayToMesh.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ConvertArrayToMesh : MonoBehaviour {
+    public float scl = 1, hScl = 0.1f;
+    ///Color[,] colorValues;
+	// Use this for initialization
+	void Start () {
+        
+        //colorValues = colors;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ArrayToMesh ()
+    {
+        Color[,] colorValues = GameObject.Find("_Manager").GetComponent<ConvertImageToArrray>().colorArray;
+        int xLength = colorValues.GetLength(0), yLength = colorValues.GetLength(1);     //get the x and y lengths of the array
+        int[] heightValues = new int[xLength*yLength];
+        int count = 0;
+        for (int j = 0; j < yLength; j++)
+        {
+            for (int i = 0; i < xLength; i++)
+            {
+                heightValues[count] = ((int)colorValues[i, j].r + (int)colorValues[i,j].g + (int)colorValues[i,j].b)/3;     //set the height value to the RGB average
+                count++;
+            }
+        }
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        mesh.vertices = Vertices(heightValues, xLength, yLength);
+
+    }
+
+    Vector3[] Vertices (int[] HeightValues, int xLength, int yLength)
+    {
+        Vector3[] vertices = new Vector3[xLength*yLength];
+        int count = 0;
+        for (int j = 0; j < yLength; j++) {
+            for (int i = 0; i < xLength; i++)
+            {
+                vertices[count] = new Vector3(xLength*scl, HeightValues[count]*hScl, yLength*scl);
+                count++;
+            }
+        }
+        
+
+        return vertices;
+    }
+}
---
+++ Vuforia-AR-App/Assets/Scripts/ConvertImageToArrray.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ConvertImageToArrray : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Color[,] colorArray;
+
+	void Start () {
+        colorArray = PNGtoArray("/Heightmaps");
+
+        foreach (Color RGB in colorArray)
+        {
+            Debug.Log(RGB);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static Color[,] PNGtoArray(string folder)
+    {
+
+        Texture2D tex = null;
+        byte[] fileData;
+        string filePath = Application.streamingAssetsPath + folder + "/Crater.png";
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData);
+            Debug.Log("worked");
+        } else
+        {
+            Debug.Log("broken");
+        }
+
+        Color[,] Array = new Color[tex.width, tex.height];
+
+        for (int i = 0; i < tex.width; i++)
+        {
+            for (int j = 0; j < tex.height; j++)
+            {
+                Debug.Log(tex.GetPixel(i, j) * 255);
+            }
+        }
+
+        return Array;
+    }
+}
